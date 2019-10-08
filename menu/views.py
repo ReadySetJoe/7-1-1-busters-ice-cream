080eef8c2be78d029c699815b7dcdd6753f1c015
ReadySetJoe <wolfgangwallaceband@gmail.com>
2019-10-08 09:12:06 -0400

    implements multiple menus

--- menu/views.py
+++ menu/views.py
@@ -7,44 +7,33 @@ from .models import IceCream
 
 def index(request):
     freshest_list = IceCream.objects.order_by('-date_churned')
-    template = loader.get_template('menu/index.html')
-    context = {
-        'freshest_list': freshest_list,
-    }
-    return HttpResponse(template.render(context, request))
+    return theRest(request, freshest_list)
+    
+
+def theRest(request, freshest_list):
+  template = loader.get_template('menu/index.html')
+  context = {
+      'freshest_list': freshest_list,
+  }
+  return HttpResponse(template.render(context, request))
+
 
 def detail(request, icecream_id):
     return HttpResponse("You're looking at %s." % icecream_id)
 
 def featured(request):
-    freshest_list = IceCream.objects.get(featured=True).order_by('-date_churned')
-    template = loader.get_template('menu/index.html')
-    context = {
-        'freshest_list': freshest_list,
-    }
-    return HttpResponse(template.render(context, request))
-
+    freshest_list = IceCream.objects.filter(featured=True).order_by('-date_churned')
+    return theRest(request, freshest_list)
 
 def daily(request):
-    freshest_list = IceCream.objects.order_by('-date_churned')[:5]
-    template = loader.get_template('menu/index.html')
-    context = {
-        'freshest_list': freshest_list,
-    }
-    return HttpResponse(template.render(context, request))
+    freshest_list = IceCream.objects.filter(available='daily').order_by('-date_churned')
+    return theRest(request, freshest_list)
 
 def weekly(request):
-    freshest_list = IceCream.objects.order_by('-date_churned')[:5]
-    template = loader.get_template('menu/index.html')
-    context = {
-        'freshest_list': freshest_list,
-    }
-    return HttpResponse(template.render(context, request))
+    freshest_list = IceCream.objects.filter(available='weekly').order_by('-date_churned')
+    return theRest(request, freshest_list)
 
 def seasonal(request):
-    freshest_list = IceCream.objects.order_by('-date_churned')[:5]
-    template = loader.get_template('menu/index.html')
-    context = {
-        'freshest_list': freshest_list,
-    }
-    return HttpResponse(template.render(context, request))
+    freshest_list = IceCream.objects.filter(available='seasonal').order_by('-date_churned')
+    return theRest(request, freshest_list)
+
